0b912a2baa09f3694b36547529fbc328bbeef7eb
r-meagher <rmeaghe2@uwo.ca>
2017-02-28 18:36:11 -0500

    Added barometer functionality

--- SensorSketch/SensorSketch.ino
+++ SensorSketch/SensorSketch.ino
@@ -1,20 +1,28 @@
 #include "Arduino.h"
 #include "Adafruit_9DOF.h"
 #include "Quadrino_GPS.h"
+#include <Adafruit_BMP085.h>
+#include <Servo.h>
+#include <Wire.h>
 
+Adafruit_BMP085 bmp;
 Adafruit_IMU imu;
 Quadrino_GPS gps;
+Servo s[7];
 
 float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
 void setup() {
+  s[0].attach(9);
   Wire.begin();
   Serial.begin(115200);
+  bmp.begin();
   imu.init();
   gps.init();
 }
 
 void loop() {
+  s[0].write(60);
   imu.update();
   gps.update();
 
@@ -39,6 +47,22 @@ void loop() {
   Serial.println(gps.lat);
   Serial.print("Longitude: ");
   Serial.println(gps.lon);
+
+  Serial.println("--------------------------------");
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+  
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+  
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.println("--------------------------------");
+  Serial.println("--------------------------------");
 
   delay(100);
 }
